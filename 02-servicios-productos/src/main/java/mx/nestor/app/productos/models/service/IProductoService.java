1df155911d4943413d4f42c94f5d3094a8a6b946
NestorMonroy <nestor.monroy.90@gmail.com>
2022-10-20 14:22:46 -0500

    [add] 2API product & [update] 1API item

---
+++ 02-servicios-productos/src/main/java/mx/nestor/app/productos/models/service/IProductoService.java
@@ -0,0 +1,12 @@
+package mx.nestor.app.productos.models.service;
+
+import java.util.List;
+
+import mx.nestor.app.productos.models.entity.Producto;
+
+public interface IProductoService {
+	
+	public List<Producto>findAll();
+	public Producto findById(Long id);
+
+}
