14d18a6cdde42e91ba41719b98ada606c3f622ce
NestorMonroy <nestor.monroy.90@gmail.com>
2022-10-23 12:22:05 -0500

    [add] example eureka server

--- 03-servicios-productos/src/main/java/mx/nestor/app/productos/Application.java
+++ 03-servicios-productos/src/main/java/mx/nestor/app/productos/Application.java
@@ -2,7 +2,9 @@ package mx.nestor.app.productos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class Application {
 
