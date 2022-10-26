84727c4103d63b580e7ecb76792040fb25ae768d
NestorMonroy <nestor.monroy.90@gmail.com>
2022-10-26 15:23:49 -0500

    [initial] project with gateway

---
+++ 04-servicio-item/src/main/java/mx/nestor/app/item/controllers/ItemController.java
@@ -0,0 +1,49 @@
+package mx.nestor.app.item.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
+import mx.nestor.app.item.models.Item;
+import mx.nestor.app.item.models.Producto;
+import mx.nestor.app.item.models.service.ItemService;
+
+@RestController
+public class ItemController {
+	
+	@Autowired
+	@Qualifier("serviceFeign")
+	private ItemService itemService;
+	
+	@GetMapping("/listar")
+	public List<Item> listar(){
+		return itemService.findAll();
+	}
+	
+	@HystrixCommand(fallbackMethod="metodoAlternativo")
+	@GetMapping("/ver/{id}/cantidad/{cantidad}")
+	public Item detalle(@PathVariable Long id, @PathVariable Integer cantidad) {
+		return itemService.findById(id, cantidad);
+	}
+	
+	public Item metodoAlternativo(Long id, Integer cantidad) {
+		Item item = new Item();
+		Producto producto = new Producto();
+		item.setCantidad(cantidad);
+		producto.setId(id);
+		producto.setNombre("Sin nombre. no encontrado!");
+		producto.setPrecio(0.00);
+		item.setProducto(producto);
+		return item;
+	}
+	
+}
+
