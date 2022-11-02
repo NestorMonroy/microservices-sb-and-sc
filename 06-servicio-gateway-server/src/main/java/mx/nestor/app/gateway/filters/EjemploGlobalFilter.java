569e26865a001528c742bfd3e42f838ba70b9486
NestorMonroy <nestor.monroy.90@gmail.com>
2022-11-02 15:46:32 -0600

    [add] config server

---
+++ 06-servicio-gateway-server/src/main/java/mx/nestor/app/gateway/filters/EjemploGlobalFilter.java
@@ -0,0 +1,67 @@
+package mx.nestor.app.gateway.filters;
+
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseCookie;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import reactor.core.publisher.Mono;
+
+/* 1-
+ * WebFlux(PROGRAMACION REACTIVA)
+ * filter: REGRESA UN MONOVOID
+ * exchange : CON ESTE OBJ SE PUEDE ACCEDER AL REQUEST Y AL RESPONSE. SE PUEDE MODIFICAR EL REQUEST Y EL RESPONSE. O REALIZAR VALIDACIONES. TAMBN SEGÚN LOS PARÁMETROS QUE SE MANDEN EN LAS CABECERAS, SE PUEDE DAR O NEGAR EL ACCESO A LOS SERVICIOS.
+ * chain : UNE EN CADENA EL FILTRO PRE CON EL POST. DESPUÉS DE chain.filter(exchange) SIGUE EL FILTRO POST.
+ * THEN() : DENTRO DEL THEN() VA (TODO EL POST) FILTER. LA RESPUESTA.
+ * MONO : UN SOLO ELEMENTO EN EL FLUJO (OBJ REALTIVO DEL TIPO MONO)
+ * Mono.fromRunnable(()..: PERMITE CREAR UN OBJ REACTIVO O MONOVOID QUE HARÁ ALGO EN LA RESPUESTA.
+ * 2- SE LE AGREGA UNA COOKIE CON EL COLOR ROJO A LA RESPUESTA.
+ * build(): CONSTRUYE EL OBJETO COOKIE.	
+ * 3 MODIFICACIÓN DEL CONTENT-TYPE A TEXTO PLANO.
+ * 4-MODIFICACIÓN DEL REQUEST CON FILTRO PRE (ANTES QUE SE ENVIE AL MICROSERVICIO O RUTA)
+ * 5- SE OBTIENE LA CABECERA DESDE EL REQUEST, PARA PODER VERLA EN EL RESPONSE.
+ * 	//getFirst(): PARA OBTENER UNA CABECERA EN PARTICULAR. EN ESTE CASO EL TOKEN.
+ * SE PREGUNTA SI ES QUE EXISTE, PARA PASAR EL TOKEN A LA CABECERA DE LA RESPUESTA. PARA ESTO, TODO SE ANIDA DENTRO DE "Optional.ofNullable".  DESPUÉS, SI ES QUE ESTÁ PRESENTE,  SE USA "ifPresent()". DENTRO DE ESTE MÉTODO SE MODIFICA LA RESPUESTA CON UNA LABDA. ASIGNANDO EL VALOR DEL TOKEN CON CLAVE Y VALOR.
+ * 
+ * */
+@Component
+public class EjemploGlobalFilter implements GlobalFilter, Ordered{
+
+	private final Logger  logger = LoggerFactory.getLogger(EjemploGlobalFilter.class);
+
+	//1
+	@Override
+	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+		logger.info("Ejecutando filtro PRE");
+		//4
+		exchange.getRequest().mutate().headers(h->h.add("token", "123456"));
+
+		return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+			logger.info("Ejecutando filtro POST");
+			
+			//5																								
+			Optional.ofNullable(exchange.getRequest().getHeaders().getFirst("token")).ifPresent(valor -> {
+				exchange.getResponse().getHeaders().add("token", valor);
+			});
+			
+			//2
+			exchange.getResponse().getCookies().add("color", ResponseCookie.from("color", "rojo").build());
+			//3
+			exchange.getResponse().getHeaders().setContentType(MediaType.TEXT_PLAIN);
+		} ));
+	}
+
+	@Override
+	public int getOrder() {
+		// TODO Auto-generated method stub
+		return 10;
+	}
+
+}
---
+++ 06-servicio-item/src/main/java/mx/nestor/app/item/AppCircuitBreakerConfig.java
@@ -0,0 +1,35 @@
+package mx.nestor.app.item;
+
+
+import java.time.Duration;
+
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
+
+@Configuration
+public class AppCircuitBreakerConfig {
+
+
+	@Bean
+	public Customizer<Resilience4JCircuitBreakerFactory> defaultCuztomizer(){
+		//1
+		return factory -> factory.configureDefault(id -> {
+			return new Resilience4JConfigBuilder(id)
+					.circuitBreakerConfig(CircuitBreakerConfig.custom()
+							.slidingWindowSize(10) //(muestreo estadistico) Número de peticiones a fallar
+							.failureRateThreshold(50) //umbral de porcentaje a falla (contabilizar las fallas, cambio de estados)
+							.waitDurationInOpenState(Duration.ofSeconds(10L))//Tiempo donde se pertenece el estado abierto (cortocircuito)
+							.permittedNumberOfCallsInHalfOpenState(5) //cambio del estado abierto a semi abierto (peticiones de prueba)
+							.slowCallRateThreshold(50) // umbral cuando las peticiones son lentas   
+							.slowCallDurationThreshold(Duration.ofSeconds(2L))//aumento de llamadas lentas 
+							.build())
+					.timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(6L)).build())
+					.build();
+		});
+	}
+}
